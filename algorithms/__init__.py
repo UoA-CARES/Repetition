ab2e49abdb269be37d6ebf0aadf8184651d1b871
hoda <hyam650@aucklanduni.ac.nz>
2026-05-26 15:46:56 +1200

    Update algorithm package exports

---
+++ algorithms/__init__.py
@@ -0,0 +1,8 @@
+from .base.td3 import TD3
+from .base.sac import SAC
+
+from .repetition.retd3 import ReTD3
+from .repetition.resac import ReSAC
+
+from .sil.sil_td3 import SILTD3
+from .sil.sil_sac import SILSAC
